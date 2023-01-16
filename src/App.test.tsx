1d62ca37b189d64c834922bfa8471bebc3a39ac3
neuralchemist <hacker.turab91@gmail.com>
2023-01-16 23:16:14 +0100

    styles(App.module.css, Scoreboard.module.css) added css styling

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/worldcup online score/i);
+  const linkElement = screen.getByText(/worldcup live score/i);
   expect(linkElement).toBeInTheDocument();
 });
